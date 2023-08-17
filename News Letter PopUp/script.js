6050746a9117a1463e44a25a561fe88b85608e71
Chitranshi Shrivastava <129734533+chitranshi962@users.noreply.github.com>
2023-08-17 19:55:48 +0530

    Add files via upload

---
+++ News Letter PopUp/script.js
@@ -0,0 +1,9 @@
+
+
+function toggleNewsletter(){
+    const newsletter = document.getElementById("newsletter");
+    newsletter.classList.toggle("active");
+}
+function isEmail(email) {
+    return /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,4}))$/.test(email);
+}
